e3d83c0bb299b25c679973671734ea02261fb1fc
龍希 宮川 <keisen_ryuki@outlook.jp>
2024-01-14 22:31:51 +0900

    鏡を追加 プレイヤの白黒を切り替える機能を実装

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,20 +6,31 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     Animator animator;  //歩行アニメーションの再生速度追加プログラム
+
+    SpriteRenderer rendererC;
+    AudioSource audioSource;
+
     float jumpForce = 300.0f;   
     float walkForce = 30.0f;    
     float maxWalkSpeed = 4.0f;
 
     bool isBallAttached = false;
+    bool isChangedColor = false;
     private GameObject attachedBall;
 
     public GameObject blackDoorLocation;
 
+    public Color playerColor;//プレイヤの色
+
+    public AudioClip MirrorSE;
+    Color tmpColor;
     void Start()
     {
         Application.targetFrameRate = 60;
         this.rigid2D = GetComponent<Rigidbody2D>();
         this.animator = GetComponent<Animator>();  //歩行アニメーションの再生速度追加プログラム
+        this.rendererC = GetComponent<SpriteRenderer>();
+        this.audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -70,29 +81,99 @@ public class PlayerController : MonoBehaviour
                     AttachBall(attachedBall);
                 }
             }
-
+        }
+        if (isChangedColor)
+        {
+            ChangeColorProcess();
         }
     }
-
     void OnTriggerEnter2D(Collider2D collision)
     {
-        //ボールだったら、ボールのオブジェクトを変数に入れる
-        if(collision.CompareTag("Ball"))
-        {
-            attachedBall = collision.gameObject;
-        }
+        /*前のプログラム
 
-        //白ドアだったら、次の指定された白ドアの場所に移動
-        if(collision.CompareTag("WhiteDoor"))
-        {
+//ボールだったら、ボールのオブジェクトを変数に入れる
+if(collision.CompareTag("Ball"))
+{
+    attachedBall = collision.gameObject;
+}
 
-        }
-        else if(collision.CompareTag("BlackDoor"))      //黒ドアだったら、次の指定された黒ドアの場所に移動
-        {
-            transform.position = blackDoorLocation.transform.position;
-        }
+//白ドアだったら、次の指定された白ドアの場所に移動
+if(collision.CompareTag("WhiteDoor"))
+{
 
+}
+else if(collision.CompareTag("BlackDoor"))      //黒ドアだったら、次の指定された黒ドアの場所に移動
+{
+    transform.position = blackDoorLocation.transform.position;
+}
+
+
+if (collision.CompareTag("Mirror"))
+{
+    if (!isChangedColor)
+    {
+        isChangedColor = true;
+        if (playerColor == Color.white)
+        {
+            playerColor = Color.black;
+        }
+        else
+        {
+            playerColor = Color.white;
+        }
+        //色を変更
+        rendererC.color = playerColor;
     }
+}
+*/
+        //提案:switch文にしたほうがよいのでは?
+        
+        switch(collision.tag)
+        {
+            //ボールだったら、ボールのオブジェクトを変数に入れる
+            case "Ball":
+                attachedBall = collision.gameObject;
+                break;
+
+            //白ドアだったら、次の指定された白ドアの場所に移動
+            case "WhiteDoor":
+                //空実装
+                break;
+
+            //黒ドアだったら、次の指定された黒ドアの場所に移動
+            case "BlackDoor":
+                transform.position = blackDoorLocation.transform.position;
+                break;
+
+            case "Mirror":
+                if (!isChangedColor)
+                {
+                    if (playerColor == Color.white)
+                    {
+                        playerColor = Color.black;
+                    }
+                    else
+                    {
+                        playerColor = Color.white;
+                    }
+                    //rendererC.color = playerColor;//即座に色を変更
+                    isChangedColor = true;
+                    this.audioSource.PlayOneShot(MirrorSE);
+                }
+            break;
+
+        }
+    }
+    /*即座に切り替える時限定
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        //トリガー方式にする
+        if (collision.CompareTag("Mirror"))
+        {
+            isChangedColor = false;
+        }
+    }
+    */
 
     //ボールと合体する
     void AttachBall(GameObject ball)
@@ -116,6 +197,33 @@ public class PlayerController : MonoBehaviour
             attachedBall.GetComponent<Rigidbody2D>().isKinematic = false;
             attachedBall = null;
         }
+    }
+
+    //色切り替えのフェード処理
+    void ChangeColorProcess()
+    {
+        if (playerColor == Color.white)
+        {
+            tmpColor += new Color(1.0f, 1.0f, 1.0f, 1.0f) * Time.deltaTime;
+            if (tmpColor.r >= playerColor.r)//r,g,bは全て同じなため
+            {
+                rendererC.color = playerColor;
+                isChangedColor = false;
+                return;
+            }
+        }
+        else
+        {
+            tmpColor -= new Color(1.0f, 1.0f, 1.0f) * Time.deltaTime;
+            if (tmpColor.r <= playerColor.r)//r,g,bは全て同じなため
+            {
+                 rendererC.color = playerColor;
+                isChangedColor = false;
+                return;
+            }
+        }
+        rendererC.color = tmpColor;
+        tmpColor.a = 1.0f;//透明度は変化させない
     }
 }
 
