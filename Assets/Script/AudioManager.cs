a37b94e2c7ef7e1125fd4e09ead79000721afeed
beth832 <150647171+beth832@users.noreply.github.com>
2024-01-20 05:02:18 +0900

    Revert "BGM added"
    This reverts commit a2abcfe64da399614c8dbfb00f5f1e7c3c79affc.

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -1,14 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource musicSource;
 
     public AudioClip BGM;
-    public static AudioManager instance;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,23 +17,11 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "EndingScene")
-        {
-            Destroy(gameObject);
-        } 
+        
     }
-   
 
     private void Awake()
     {
-        if (instance != null)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        } 
+        DontDestroyOnLoad(gameObject);
     }
 }
